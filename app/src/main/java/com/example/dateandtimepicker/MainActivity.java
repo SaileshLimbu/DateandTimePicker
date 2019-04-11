a0c10874bab59fc49ca6a64cfcb4377f2da6f616
Softwarica College Lab <softwarica@softwarica.com>
2019-04-11 10:49:40 +0545

    Initial commit

---
+++ app/src/main/java/com/example/dateandtimepicker/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.dateandtimepicker;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    private TextView tvDate, tvTime;
+    private Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDate = findViewById(R.id.tvDate);
+        tvTime = findViewById(R.id.tvTime);
+        calendar = Calendar.getInstance();
+
+        tvDate.setOnClickListener(this);
+        tvTime.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.tvDate:
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePicker = new DatePickerDialog(this, this, year, month, day);
+                datePicker.show();
+                break;
+            case R.id.tvTime:
+                int hour = calendar.get(Calendar.HOUR);
+                int minute = calendar.get(Calendar.MINUTE);
+                TimePickerDialog dialog = new TimePickerDialog(this, this, hour, minute, false);
+                dialog.show();
+                break;
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        DateFormat format = new SimpleDateFormat("dd MMMM YYYY");
+        String date =format.format(new Date(year, month, dayOfMonth));
+        tvDate.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String time = "Time : " + hourOfDay + ":" + minute;
+        tvTime.setText(time);
+    }
+}
